c61c546de95793cca01705192875e1d9d8b50dfd
Achyudhan <akutuva22@gmail.com>
2022-06-24 17:42:29 -0400

    Added a short program to graph basic relationships

---
+++ patient_sample_graph.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib import style, cm, colors
+
+s = '../SPARK Project/All_Values.csv'
+t = pd.read_csv(str(s))
+
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+fig.suptitle('Something', fontsize=20)
+plt.xlabel('Alpha', fontsize=18)
+plt.ylabel('Cumulative Dose', fontsize=16)
+plt.xticks(fontsize = 15)
+plt.yticks(fontsize = 15)
+
+lam = t.iloc[:,0]
+alpha = t.iloc[:,1]
+delta = t.iloc[:,2]
+psi = t.iloc[:,3]
+cumul = t.iloc[:,4]
+frac_size = t.iloc[:,5]
+num = cumul / frac_size
+gamma = 1 - np.exp(-alpha * frac_size - (alpha / 10) * pow(frac_size, 2))
+
+ax.scatter3D(alpha, cumul, psi, s = 1)
+
+plt.show()
